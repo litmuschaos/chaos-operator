6b081a0aab4923450a719d191b305630c7bc87a7
Amit Bhatt <amitbhatt818@gmail.com>
2019-09-03 19:23:10 +0530

    add a struct to hold app info
    Signed-off-by: Amit Bhatt <amitbhatt818@gmail.com>

--- pkg/controller/chaosengine/chaosengine_controller.go
+++ pkg/controller/chaosengine/chaosengine_controller.go
@@ -101,6 +101,23 @@ type ReconcileChaosEngine struct {
 	client client.Client
 	scheme *runtime.Scheme
 }
+type applicationInfo struct {
+	label     map[string]string
+	namespace string
+	expList   []litmuschaosv1alpha1.ExperimentList
+	lKey      string
+	lValue    string
+}
+
+func (a *applicationInfo) Initialize(instance *litmuschaosv1alpha1.ChaosEngine) *applicationInfo {
+	aLabelKeyValue := strings.Split(instance.Spec.Appinfo.Applabel, "=")
+	lKey := aLabelKeyValue[0]
+	lValue := aLabelKeyValue[1]
+	a.label[lKey] = lValue
+	a.namespace = instance.Spec.Appinfo.Appns
+	a.expList = instance.Spec.Experiments
+	return a
+}
 
 // Reconcile reads that state of the cluster for a ChaosEngine object and makes changes based on the state read
 // and what is in the ChaosEngine.Spec
@@ -131,16 +148,11 @@ func (r *ReconcileChaosEngine) Reconcile(request reconcile.Request) (reconcile.R
 	// TODO: Get app kind from chaosengine spec as well. Using "deploy" for now
 	// TODO: Freeze label format in chaosengine( "=" as a const)
 
-	aLabelKeyValue := strings.Split(instance.Spec.Appinfo.Applabel, "=")
-	lKey := aLabelKeyValue[0]
-	lValue := aLabelKeyValue[1]
-	aLabel := make(map[string]string)
-	aLabel[lKey] = lValue
-	aNamespace := instance.Spec.Appinfo.Appns
-	aExpList := instance.Spec.Experiments
+	a := &applicationInfo{}
+	a = a.Initialize(instance)
 
 	var appExperiments []string
-	for _, exp := range aExpList {
+	for _, exp := range a.expList {
 		appExperiments = append(appExperiments, exp.Name)
 	}
 
@@ -170,7 +182,7 @@ func (r *ReconcileChaosEngine) Reconcile(request reconcile.Request) (reconcile.R
 		return reconcile.Result{}, err
 	}
 
-	cApp, err := clientset.AppsV1().Deployments(aNamespace).List(metav1.ListOptions{LabelSelector: fmt.Sprintf("%s=%s", lKey, lValue), FieldSelector: ""})
+	cApp, err := clientset.AppsV1().Deployments(a.namespace).List(metav1.ListOptions{LabelSelector: fmt.Sprintf("%s=%s", a.lKey, a.lValue), FieldSelector: ""})
 	if err != nil {
 		//logrus.Fatal("Failed to list deployments. Error is ", err)
 		log.Error(err, "unable to list apps matching labels")
