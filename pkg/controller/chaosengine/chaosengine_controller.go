9643c748822ecafe24e58e096fa41597474ac775
Amit Bhatt <amitbhatt818@gmail.com>
2019-09-04 19:28:03 +0530

    improved code
    Signed-off-by: Amit Bhatt <amitbhatt818@gmail.com>

--- pkg/controller/chaosengine/chaosengine_controller.go
+++ pkg/controller/chaosengine/chaosengine_controller.go
@@ -102,20 +102,22 @@ type ReconcileChaosEngine struct {
 	scheme *runtime.Scheme
 }
 type applicationInfo struct {
-	label     map[string]string
-	namespace string
-	expList   []litmuschaosv1alpha1.ExperimentList
-	lKey      string
-	lValue    string
+	namespace      string
+	label          map[string]string
+	experimentList []litmuschaosv1alpha1.ExperimentList
 }
+
+var labelKey string
+var labelValue string
 
 func (a *applicationInfo) Initialize(instance *litmuschaosv1alpha1.ChaosEngine) *applicationInfo {
 	aLabelKeyValue := strings.Split(instance.Spec.Appinfo.Applabel, "=")
-	lKey := aLabelKeyValue[0]
-	lValue := aLabelKeyValue[1]
-	a.label[lKey] = lValue
+	labelKey = aLabelKeyValue[0]
+	labelValue = aLabelKeyValue[1]
+	a.label = make(map[string]string)
+	a.label[labelKey] = labelValue
 	a.namespace = instance.Spec.Appinfo.Appns
-	a.expList = instance.Spec.Experiments
+	a.experimentList = instance.Spec.Experiments
 	return a
 }
 
@@ -152,7 +154,7 @@ func (r *ReconcileChaosEngine) Reconcile(request reconcile.Request) (reconcile.R
 	a = a.Initialize(instance)
 
 	var appExperiments []string
-	for _, exp := range a.expList {
+	for _, exp := range a.experimentList {
 		appExperiments = append(appExperiments, exp.Name)
 	}
 
@@ -163,8 +165,9 @@ func (r *ReconcileChaosEngine) Reconcile(request reconcile.Request) (reconcile.R
 	   logrus.Info("Exp list derived from chaosengine is ", appExperiments)
 	*/
 
-	log.Info("App Label derived from Chaosengine is ", "appLabel", aLabel)
-	log.Info("App NS derived from Chaosengine is ", "appNamespace", aNamespace)
+	log.Info("App key derived from chaosengine is ", "appLabelKey", labelKey)
+	log.Info("App Label derived from Chaosengine is ", "appLabelValue", labelValue)
+	log.Info("App NS derived from Chaosengine is ", "appNamespace", a.namespace)
 	log.Info("Exp list derived from chaosengine is ", "appExpirements", appExperiments)
 
 	// Use client-Go to obtain a list of apps w/ specified labels
@@ -182,7 +185,7 @@ func (r *ReconcileChaosEngine) Reconcile(request reconcile.Request) (reconcile.R
 		return reconcile.Result{}, err
 	}
 
-	cApp, err := clientset.AppsV1().Deployments(a.namespace).List(metav1.ListOptions{LabelSelector: fmt.Sprintf("%s=%s", a.lKey, a.lValue), FieldSelector: ""})
+	cApp, err := clientset.AppsV1().Deployments(a.namespace).List(metav1.ListOptions{LabelSelector: fmt.Sprintf("%s=%s", labelKey, labelValue), FieldSelector: ""})
 	if err != nil {
 		//logrus.Fatal("Failed to list deployments. Error is ", err)
 		log.Error(err, "unable to list apps matching labels")
