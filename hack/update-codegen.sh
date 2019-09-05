6c3c863aa3471b191359a5d9e63de74fb5d47744
chandan kumar <chandan.kumar@mayadata.io>
2019-09-05 17:31:05 +0530

    Generate k8s clientset
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>

---
+++ hack/update-codegen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+${GOPATH}/src/k8s.io/code-generator/generate-groups.sh client,lister,informer \
+  github.com/litmuschaos/chaos-operator/pkg/client github.com/litmuschaos/chaos-operator/pkg/apis \
+  litmuschaos:v1alpha1 
+
