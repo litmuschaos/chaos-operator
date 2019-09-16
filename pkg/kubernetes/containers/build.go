b5e614e86a376c385daa4ff3cd94e27654857090
ksatchit <ksatchit@mayadata.io>
2019-09-16 16:06:33 +0530

    (refactor)controller: provide image pull policy builder function
    Signed-off-by: ksatchit <ksatchit@mayadata.io>

--- pkg/kubernetes/containers/build.go
+++ pkg/kubernetes/containers/build.go
@@ -63,6 +63,22 @@ func (b *Builder) WithImage(img string) *Builder {
 	return b
 }
 
+// WithImagePullPolicy sets the image pull policy of the container
+func (b *Builder) WithImagePullPolicy(policy corev1.PullPolicy) *Builder {
+	if len(policy) == 0 {
+		b.errors = append(
+			b.errors,
+			errors.New(
+				"failed to build container object: missing imagepullpolicy",
+			),
+		)
+		return b
+	}
+
+	b.con.object.ImagePullPolicy = policy
+	return b
+}
+
 // WithCommandNew sets the command of the container
 func (b *Builder) WithCommandNew(cmd []string) *Builder {
 	if cmd == nil {
