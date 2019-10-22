22d3fd42a177603d59f1c6977501298e0d3e4aa4
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2019-10-22 19:26:35 +0530

    update(unit tests): add image field in unittest
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- pkg/controller/chaosengine/chaosengine_controller_test.go
+++ pkg/controller/chaosengine/chaosengine_controller_test.go
@@ -23,6 +23,14 @@ func TestNewRunnerPodForCR(t *testing.T) {
 					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
 						ChaosServiceAccount: "fake-serviceAccount",
 						Monitoring:          true,
+						Components: litmuschaosv1alpha1.ComponentParams{
+							Monitor: litmuschaosv1alpha1.MonitorInfo{
+								Image: "fake-monitor-image",
+							},
+							Runner: litmuschaosv1alpha1.RunnerInfo{
+								Image: "fake-runner-image",
+							},
+						},
 					},
 				},
 				appUUID:        "fake_id",
@@ -40,6 +48,14 @@ func TestNewRunnerPodForCR(t *testing.T) {
 					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
 						ChaosServiceAccount: "fake-serviceAccount",
 						Monitoring:          false,
+						Components: litmuschaosv1alpha1.ComponentParams{
+							Monitor: litmuschaosv1alpha1.MonitorInfo{
+								Image: "fake-monitor-image",
+							},
+							Runner: litmuschaosv1alpha1.RunnerInfo{
+								Image: "fake-runner-image",
+							},
+						},
 					},
 				},
 				appUUID:        "fake_id",
@@ -90,6 +106,30 @@ func TestNewRunnerPodForCR(t *testing.T) {
 			},
 			isErr: true,
 		},
+		"Test Negative-4 ": {
+			engine: engineInfo{
+				instance: &litmuschaosv1alpha1.ChaosEngine{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-runner",
+						Namespace: "test",
+					},
+					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
+						ChaosServiceAccount: "fake-serviceAccount",
+						Components: litmuschaosv1alpha1.ComponentParams{
+							Monitor: litmuschaosv1alpha1.MonitorInfo{
+								Image: "",
+							},
+							Runner: litmuschaosv1alpha1.RunnerInfo{
+								Image: "",
+							},
+						},
+					},
+				},
+				appUUID:        "fake_id",
+				appExperiments: []string{},
+			},
+			isErr: true,
+		},
 	}
 	for name, mock := range tests {
 		name, mock := name, mock
@@ -119,6 +159,14 @@ func TestNewMonitorServiceForCR(t *testing.T) {
 					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
 						ChaosServiceAccount: "fake-serviceAccount",
 						Monitoring:          false,
+						Components: litmuschaosv1alpha1.ComponentParams{
+							Monitor: litmuschaosv1alpha1.MonitorInfo{
+								Image: "fake-monitor-image",
+							},
+							Runner: litmuschaosv1alpha1.RunnerInfo{
+								Image: "fake-runner-image",
+							},
+						},
 					},
 				},
 			},
@@ -165,6 +213,14 @@ func TestNewMonitorPodForCR(t *testing.T) {
 					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
 						ChaosServiceAccount: "fake-serviceAccount",
 						Monitoring:          false,
+						Components: litmuschaosv1alpha1.ComponentParams{
+							Monitor: litmuschaosv1alpha1.MonitorInfo{
+								Image: "fake-monitor-image",
+							},
+							Runner: litmuschaosv1alpha1.RunnerInfo{
+								Image: "fake-runner-image",
+							},
+						},
 					},
 				},
 			},
