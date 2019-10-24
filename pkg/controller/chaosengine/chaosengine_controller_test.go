25a723920c9c86ea4f2d7c32900d88fba44f7f87
Rayan Das <rayandas91@gmail.com>
2019-10-24 14:41:35 +0530

    added unit test for add()

--- pkg/controller/chaosengine/chaosengine_controller_test.go
+++ pkg/controller/chaosengine/chaosengine_controller_test.go
@@ -8,6 +8,57 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func Testadd(t *testing.T)
+{
+	tests := map[string]struct 
+	{
+		engine engineInfo
+		isErr  bool
+	}
+	{
+		"Test Positive": {
+			engine: engineInfo{
+				instance: &litmuschaosv1alpha1.ChaosEngine{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-monitor",
+						Namespace: "test",
+					},
+					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
+						ChaosServiceAccount: "fake-serviceAccount",
+						Monitoring:          false,
+					},
+				},
+			},
+			isErr: false,
+		},
+		"Test Negative": {
+			engine: engineInfo{
+				instance: &litmuschaosv1alpha1.ChaosEngine{
+					ObjectMeta: metav1.ObjectMeta{},
+					Spec: litmuschaosv1alpha1.ChaosEngineSpec{
+						Monitoring: true,
+					},
+				},
+			},
+			isErr: true,
+		},
+	}
+	for name, mock := range tests {
+		name, mock := name, mock
+		t.Run(name, func(t *testing.T) {
+
+			_, err := Add(mock.engine)
+			if mock.isErr && err == nil {
+				t.Fatalf("Test %q failed: expected error not to be nil", name)
+			}
+			if !mock.isErr && err != nil {
+				t.Fatalf("Test %q failed: expected error to be nil", name)
+			}
+		})
+	}
+}
+// End Add() unit test
+
 func TestNewRunnerPodForCR(t *testing.T) {
 	tests := map[string]struct {
 		engine engineInfo
