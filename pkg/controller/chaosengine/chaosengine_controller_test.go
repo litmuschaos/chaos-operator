20b0440b62dfad6a051cafba66d9c66948526d92
shubhamchaudhary <shubham.chaudhary@mayadata.io>
2019-11-11 14:40:43 +0530

    (fix): update appns field in runner-pod
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- pkg/controller/chaosengine/chaosengine_controller_test.go
+++ pkg/controller/chaosengine/chaosengine_controller_test.go
@@ -5,11 +5,12 @@ import (
 	"strings"
 	"testing"
 
-	litmuschaosv1alpha1 "github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1"
-	chaosTypes "github.com/litmuschaos/chaos-operator/pkg/controller/types"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+
+	litmuschaosv1alpha1 "github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1"
+	chaosTypes "github.com/litmuschaos/chaos-operator/pkg/controller/types"
 )
 
 func TestNewRunnerPodForCR(t *testing.T) {
