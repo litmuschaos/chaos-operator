c583f974c8994c3b6e2cc265b8ca9e9c520d4872
Amit Bhatt <33670159+amitbhatt818@users.noreply.github.com>
2019-11-25 14:29:16 +0530

    Added operator default ENV for chaos runner/monitor image (#96)
    * Added operator default ENV for chaos runner/monitor image
    
    Signed-off-by: Amit Bhatt <amitbhatt818@gmail.com>

--- pkg/controller/types/types.go
+++ pkg/controller/types/types.go
@@ -48,11 +48,11 @@ var (
 	// Log with default name ie: controller_chaosengine
 	Log = logf.Log.WithName("controller_chaosengine")
 
-	// DefaultRunnerImage contains the default value of runner resource
-	DefaultRunnerImage = "litmuschaos/ansible-runner:latest"
+	// DefaultChaosMonitorImage contains the default value of monitor resource
+	DefaultChaosMonitorImage = "litmuschaos/chaos-exporter:latest"
 
-	// DefaultMonitorImage contains the default value of monitor resource
-	DefaultMonitorImage = "litmuschaos/chaos-exporter:latest"
+	// DefaultChaosRunnerImage contains the default value of runner resource
+	DefaultChaosRunnerImage = "litmuschaos/ansible-runner:latest"
 )
 
 // ApplicationInfo contains the chaos details for target application
