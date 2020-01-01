7bfb1e62be1ef66a30696b0a46688702a2876724
chandan kumar <chandan.kr404@gmail.com>
2020-01-01 22:51:31 +0530

    Refactor the main file
    Signed-off-by: chandan kumar <chandan.kr404@gmail.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -36,6 +36,7 @@ import (
 	"github.com/spf13/pflag"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -48,40 +49,33 @@ import (
 var (
 	metricsHost       = "0.0.0.0"
 	metricsPort int32 = 8383
+	log               = logf.Log.WithName("cmd")
 )
-var log = logf.Log.WithName("cmd")
-
-func printVersion() {
-	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
-	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
-	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
-}
-
-func registerComponents(cfg *rest.Config, namespace string) (manager.Manager, error) {
-
-	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := manager.New(cfg, manager.Options{Namespace: namespace, MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort)})
-	if err != nil {
-		return mgr, err
-	}
-
-	log.Info("Registering Components.")
-
-	// Setup Scheme for all resources
-	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
-		return mgr, err
-	}
-
-	// Setup all Controllers
-	if err := controller.AddToManager(mgr); err != nil {
-		return mgr, err
-	}
-
-	return mgr, err
-}
 
 func main() {
+	// initializing the log configuration
+	initializingLogConfiguration()
 
+	// printing the operator and go configuration
+	printVersion()
+
+	// setting up initial configuration of chaos-operator
+	mgr, err := initialConfiguration()
+	if err != nil {
+		log.Error(err, "")
+		os.Exit(1)
+	}
+
+	// Start the Chaos-Operator
+	log.Info("Starting the Chaos-Operator...")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "Chaos-Operator exited non-zero")
+		os.Exit(1)
+	}
+}
+
+// initialize the log configuration
+func initializingLogConfiguration() {
 	// Add the zap logger flag set to the CLI. The flag set must
 	// be added before calling pflag.Parse().
 	pflag.CommandLine.AddFlagSet(zap.FlagSet())
@@ -101,60 +95,88 @@ func main() {
 	// be propagated through the whole operator, generating
 	// uniform and structured logs.
 	logf.SetLogger(zap.Logger())
+}
 
-	printVersion()
+func printVersion() {
+	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
+	log.Info(fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH))
+	log.Info(fmt.Sprintf("Version of operator-sdk: %v", sdkVersion.Version))
+}
 
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		log.Error(err, "Failed to get watch namespace")
-		os.Exit(1)
+// initializing the configuration of chaos-operator
+func initialConfiguration() (manager.Manager, error) {
+	//setting up leader and analytics
+	if err := initializingLeaderAndAnalytics(); err != nil {
+		return nil, err
 	}
 
-	// Get a config to talk to the apiserver
-	cfg, err := config.GetConfig()
+	// creating metrics service
+	cfg, namespace, err := initializeMetricsService()
 	if err != nil {
-		log.Error(err, "")
-		os.Exit(1)
+		return nil, err
 	}
 
-	ctx := context.TODO()
+	// registering the components of chaos-operator
+	mgr, err := registerComponents(cfg, namespace)
+	if err != nil {
+		return nil, err
+	}
+	return mgr, nil
+}
 
+func initializingLeaderAndAnalytics() error {
 	// Become the leader before proceeding
-	err = leader.Become(ctx, "chaos-operator-lock")
-	if err != nil {
-		log.Error(err, "")
-		os.Exit(1)
+	if err := leader.Become(context.TODO(), "chaos-operator-lock"); err != nil {
+		return err
 	}
 
-	isAnalytics := strings.ToUpper(os.Getenv("ANALYTICS"))
-	// Check if the TrackingStatus env has been negated or not
-	if isAnalytics != "FALSE" {
-		err := analytics.TriggerAnalytics()
-		if err != nil {
+	// Trigger the Analytics if it's enabled
+	if isAnalytics := strings.ToUpper(os.Getenv("ANALYTICS")); isAnalytics != "FALSE" {
+		if err := analytics.TriggerAnalytics(); err != nil {
 			log.Error(err, "")
 		}
 	}
 
-	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := registerComponents(cfg, namespace)
+	return nil
+}
+
+func initializeMetricsService() (*rest.Config, string, error) {
+	// Get a config to talk to the apiserver
+	cfg, err := config.GetConfig()
 	if err != nil {
-		log.Error(err, "")
-		os.Exit(1)
+		return cfg, "", err
 	}
+
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to get watch namespace %v", err)
+	}
+
 	// Create Service object to expose the metrics port(s).
-	servicePorts := []v1.ServicePort{
-		{Port: metricsPort, Name: metrics.OperatorPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}},
-	}
-	_, err = metrics.CreateMetricsService(ctx, cfg, servicePorts)
-	if err != nil {
+	servicePorts := []v1.ServicePort{{Port: metricsPort, Name: metrics.OperatorPortName, Protocol: v1.ProtocolTCP, TargetPort: intstr.IntOrString{Type: intstr.Int, IntVal: metricsPort}}}
+	if _, err := metrics.CreateMetricsService(context.TODO(), cfg, servicePorts); err != nil {
 		log.Info("Could not create metrics Service", "error", err.Error())
 	}
+	return cfg, namespace, nil
+}
 
-	log.Info("Starting the Chaos-Operator...")
+func registerComponents(cfg *rest.Config, namespace string) (manager.Manager, error) {
 
-	// Start the Chaos-Operator
-	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		log.Error(err, "Manager exited non-zero")
-		os.Exit(1)
+	// Create a new Cmd to provide shared dependencies and start components
+	mgr, err := manager.New(cfg, manager.Options{Namespace: namespace, MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort)})
+	if err != nil {
+		return mgr, err
 	}
+
+	// Setup Scheme for all resources
+	if err := apis.AddToScheme(mgr.GetScheme()); err != nil {
+		return nil, err
+	}
+
+	// Setup all Controllers
+	if err := controller.AddToManager(mgr); err != nil {
+		return nil, err
+	}
+
+	return mgr, nil
 }
