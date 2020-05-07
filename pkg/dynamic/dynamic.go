7901608024955479ce0cceafe3d4338e86170a32
Raj Babu Das <mail.rajdas@gmail.com>
2020-05-07 15:54:45 +0530

    (feat): Adding dynamic client & deploymentconfig object support (#199)
    * Adding deploymentconfig annotation check support
    
    Signed-off-by: Raj <raj.das@mayadata.io>

---
+++ pkg/dynamic/dynamic.go
@@ -0,0 +1,18 @@
+package dynamic
+
+import (
+	"k8s.io/client-go/dynamic"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+func CreateClientSet() (*dynamic.Interface, error){
+	restConfig, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientSet, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+	return &clientSet, nil
+}
