520679f26f3d8b8e3c804eb475a67910ad9e4768
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2020-11-13 23:55:37 +0530

    chore(onChaos-Mode): Adding onChaos mode in probes (#296)
    Signed-off-by: shubhamchaudhary <shubham.chaudhary@mayadata.io>

--- pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
+++ pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
@@ -264,6 +264,8 @@ type RunProperty struct {
 	//ProbePollingInterval contains time interval, for which continuous probe should be sleep
 	// after each iteration
 	ProbePollingInterval int `json:"probePollingInterval,omitempty"`
+	//InitialDelaySeconds time interval for which probe will wait before run
+	InitialDelaySeconds int `json:"initialDelaySeconds,omitempty"`
 }
 
 // ExperimentComponents contains ENV, Configmaps and Secrets
