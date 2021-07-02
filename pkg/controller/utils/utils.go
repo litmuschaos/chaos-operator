3c09ab02a987b480dba656d752e9aa831b8d7aba
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2021-07-02 08:35:22 +0530

    feat(analytics): using same user-id for anaytics even if operator pod restarts (#372)
    Signed-off-by: shubhamchaudhary <shubham@chaosnative.com>

--- pkg/controller/utils/utils.go
+++ pkg/controller/utils/utils.go
@@ -16,6 +16,10 @@ limitations under the License.
 
 package utils
 
+import (
+	corev1 "k8s.io/api/core/v1"
+)
+
 // RemoveString removes a particular string from a slice of strings
 func RemoveString(slice []string, s string) (result []string) {
 	for _, item := range slice {
@@ -25,4 +29,15 @@ func RemoveString(slice []string, s string) (result []string) {
 		result = append(result, item)
 	}
 	return
+}
+
+// SetEnv sets the env inside envDetails struct
+func (envDetails *ENVDetails) SetEnv(key, value string) *ENVDetails {
+	if value != "" {
+		envDetails.ENV = append(envDetails.ENV, corev1.EnvVar{
+			Name:  key,
+			Value: value,
+		})
+	}
+	return envDetails
 }
