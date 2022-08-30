06844b24ed0ee54ff011abdb6b6db41e1e470acb
Shubham Chaudhary <shubham.chaudhary@harness.io>
2022-08-30 23:14:03 +0530

    chore(leader-elect): enabling leader-elect for multiple replicas
    Signed-off-by: Shubham Chaudhary <shubham.chaudhary@harness.io>

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "2a501fa0.",
+		LeaderElectionID:       "chaos-operator.lock",
 		Namespace:              namespace,
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
