0d612acdeb577ab9abc310e08c323369b81e5396
Shovan Maity <shovan.maity@harness.io>
2024-03-07 11:47:26 +0530

    add fuzz test for set env utility function
    Signed-off-by: Shovan Maity <shovan.maity@harness.io>

--- pkg/utils/utils_fuzz_test.go
+++ pkg/utils/utils_fuzz_test.go
@@ -17,27 +17,29 @@ limitations under the License.
 package utils
 
 import (
-	corev1 "k8s.io/api/core/v1"
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
 )
 
-// RemoveString removes a particular string from a slice of strings
-func RemoveString(slice []string, s string) (result []string) {
-	for _, item := range slice {
-		if item == s {
-			continue
+func FuzzSetEnv(f *testing.F) {
+	kv := map[string]string{
+		"KEY1": "VALUE1",
+		"KEY2": "VALUE2",
+	}
+	for k, v := range kv {
+		f.Add(k, v)
+	}
+	f.Fuzz(func(t *testing.T, key, value string) {
+		ed := ENVDetails{
+			ENV: make([]v1.EnvVar, 0),
 		}
-		result = append(result, item)
-	}
-	return
-}
-
-// SetEnv sets the env inside envDetails struct
-func (envDetails *ENVDetails) SetEnv(key, value string) *ENVDetails {
-	if key != "" && value != "" {
-		envDetails.ENV = append(envDetails.ENV, corev1.EnvVar{
-			Name:  key,
-			Value: value,
-		})
-	}
-	return envDetails
+		edUpdated := ed.SetEnv(key, value)
+		if edUpdated == nil {
+			t.Error("nil object not expected")
+		}
+		if key != "" && value != "" && edUpdated != nil && len(edUpdated.ENV) != 1 {
+			t.Errorf("expected env to be available, len %d", len(edUpdated.ENV))
+		}
+	})
 }
